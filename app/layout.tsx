c2280c1db315f583f2412ffe1e1292deeec1c901
MariusDevelops <brazasmarius@gmail.com>
2025-01-05 22:38:57 +0200

    feat: footer setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Navbar } from "./components/nav";
+import Footer from "./components/footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -32,6 +33,7 @@ export default function RootLayout({
         <main className="flex-auto min-w-0 mt-2 md:mt-6 flex flex-col px-6 sm:px-4 md:px-0 max-w-[640px] w-full">
           <Navbar />
           {children}
+          <Footer />
         </main>
       </body>
     </html>
